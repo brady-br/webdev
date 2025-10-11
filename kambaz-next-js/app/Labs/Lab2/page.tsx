7b8672bca2da36ec0c9fd0e2aeeac39cad10ecb8
brady-br <91754190+brady-br@users.noreply.github.com>
2025-10-11 15:25:37 -0400

    Implemented 2.3

--- kambaz-next-js/app/Labs/Lab2/page.tsx
+++ kambaz-next-js/app/Labs/Lab2/page.tsx
@@ -12,10 +12,17 @@ import Float from "./Float";
 import GridLayout from "./GridLayout";
 import Flex from "./Flex";
 import ReactIconsSampler from "./ReactIcons";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
+import Container from "react-bootstrap/Container";
 
 export default function Lab2() {
   return (
-    <div id="wd-lab2">
+    <Container id="wd-lab2">
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <p>
@@ -79,5 +86,11 @@ Here's another paragraph using a different ID and a different look and
       <GridLayout />
       <Flex />
       <ReactIconsSampler />
-    </div>)
+      <BootstrapGrids />
+      <ScreenSizeLabel />
+      <BootstrapTables />
+      <BootstrapLists />
+      <BootstrapForms />
+      <BootstrapNavigation />
+    </Container>)
 ;}
