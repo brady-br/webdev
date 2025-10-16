fa1070c5636ebc2657d028338fc3dcbe7c9a72be
brady-br <91754190+brady-br@users.noreply.github.com>
2025-10-16 11:20:46 -0400

    Implemented 3.6

--- kambaz-next-js/app/Labs/Lab3/MapFunction.tsx
+++ kambaz-next-js/app/Labs/Lab3/MapFunction.tsx
@@ -12,7 +12,7 @@ export default function MapFunction() {
       Todos:
       <ol>
         {todos.map((todo) => (
-          <li>{todo}</li>
+          <li key={`todo${todos.findIndex((item) => todo === item)}`}>{todo}</li>
         ))}
       </ol> <hr/>
     </div>
