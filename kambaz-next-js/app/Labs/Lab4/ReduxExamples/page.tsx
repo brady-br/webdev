7fb4b8bb692349bc5e67082ae596b6f1b713339d
brady-br <91754190+brady-br@users.noreply.github.com>
2025-11-22 15:31:47 -0500

    Disabled ReduxExample pre-render

--- kambaz-next-js/app/Labs/Lab4/ReduxExamples/page.tsx
+++ kambaz-next-js/app/Labs/Lab4/ReduxExamples/page.tsx
@@ -3,8 +3,12 @@ import HelloRedux from "./HelloRedux";
 import CounterRedux from "./CounterRedux";
 import AddRedux from "./AddRedux";
 import TodoList from "./todos/TodoList";
+import { useState, useEffect } from "react";
 
 export default function ReduxExamples() {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => setIsClient(true));
+  if (!isClient) return null;
   return(
     <div>
       <h2>Redux Examples</h2>
